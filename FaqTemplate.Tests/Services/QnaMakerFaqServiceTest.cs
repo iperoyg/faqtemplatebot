9cbdfc6f282b9fbf1db420fa2be5e2847ccd95c3
Arthur Iperoyg <iperoyg@gmail.com>
2017-01-08 10:43:53 -0200

    Add test project

---
+++ FaqTemplate.Tests/Services/QnaMakerFaqServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaqTemplate.Tests.Services
+{
+    [TestFixture]
+    public class QnaMakerFaqServiceTest 
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void DoNothing()
+        {
+            Assert.That(null, Is.Null);
+        }
+    }
+}
