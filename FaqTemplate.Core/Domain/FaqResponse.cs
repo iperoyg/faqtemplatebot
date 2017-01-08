845ce75073cfcaa7deff6419436fb7fbfa671f22
Arthur Iperoyg <iperoyg@gmail.com>
2017-01-08 08:54:03 -0200

    First Commit

--- FaqTemplate.Core/Domain/FaqResponse.cs
+++ FaqTemplate.Core/Domain/FaqResponse.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FaqTemplateBot
+namespace FaqTemplate.Core.Domain
 {
-    public class Class1
+    class FaqResponse
     {
     }
 }
