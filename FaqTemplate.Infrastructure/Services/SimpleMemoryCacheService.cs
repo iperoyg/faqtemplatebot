b642d2dba85bfa6231069f8ee43b02f972c88ed3
Arthur Iperoyg <iperoyg@gmail.com>
2017-05-20 18:29:03 -0300

    Add implementation for simple cache using Dictionary

--- FaqTemplate.Infrastructure/Services/SimpleMemoryCacheService.cs
+++ FaqTemplate.Infrastructure/Services/SimpleMemoryCacheService.cs
@@ -9,24 +9,37 @@ namespace FaqTemplate.Infrastructure.Services
 {
     public class SimpleMemoryCacheService<T> : ICacheService<T>
     {
+        private IDictionary<string, T> _cache;
+
+        public SimpleMemoryCacheService()
+        {
+            _cache = new Dictionary<string, T>();
+        }
+
         public Task AddOrUpdate(string key, T value)
         {
+            _cache.Remove(key);
+            _cache.Add(key, value);
             return Task.CompletedTask;
         }
 
         public Task Clear()
         {
+            _cache.Clear();
             return Task.CompletedTask;
         }
 
         public Task<T> Get(string key)
         {
-            return Task.FromResult(default(T));
+            var data = _cache.ContainsKey(key) ? _cache[key] : default(T);
+            return Task.FromResult(data);
         }
 
         public Task<T> Remove(string key)
         {
-            return Task.FromResult(default(T));
+            var data = _cache.ContainsKey(key) ? _cache[key] : default(T);
+            _cache.Remove(key);
+            return Task.FromResult(data);
         }
     }
 }
